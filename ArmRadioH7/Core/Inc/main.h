9eaf4ec5604f2f73489589ebefce319ab080d08b
Alberto Garlassi <alberto-g@inventati.org>
2020-05-26 11:56:57 +0200

    Some cleaning

---
+++ ArmRadioH7/Core/Inc/main.h
@@ -0,0 +1,207 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+
+#include <arm_math.h>
+#include "SDR_math.h"
+//#include "Globals.h"
+
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+//typedef uint8_t    u8;
+typedef enum {AM=0, LSB=1, USB=2, CW=3} Mode;
+typedef enum {Fast, Slow}   Agctype;
+typedef enum {Narrow, Wide} Bwidth;
+typedef struct
+        {
+          char   name[16];
+          float  freq;
+          Mode   mode;
+          Bwidth bw;
+        } Presets;
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+#define LED_YELLOW_OFF    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_1, GPIO_PIN_RESET)
+#define LED_YELLOW_ON   HAL_GPIO_WritePin(GPIOE, GPIO_PIN_1, GPIO_PIN_SET)
+#define LED_RED_OFF  	 HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET)
+#define LED_RED_ON  	 HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_SET)
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+//void SDR_ADC_StructInit(SDR_ADC_InitTypeDef* ADC_InitStruct);
+
+//extern void Set_NVIC_PriorityGroupConfig(uint32_t PriorityGroup);
+//extern void NVIC_Init(NVIC_InitTypeDef* NVIC_InitStruct);
+//extern void SDR_ADC_Init(ADC_TypeDef* ADCx, SDR_ADC_InitTypeDef* ADC_InitStruct);
+
+//extern void SDR_InitGPIO(void);
+//extern void SDR_InitEXTI(void);
+//extern void SDR_InitDAC(void);
+//extern void SDR_InitADC(void);
+//extern void SDR_StartADC(void);
+//extern void SDR_InitEXTI(void);
+extern void Tune_Preset(uint8_t);
+extern void Load_Presets(void);
+extern void DisplayFrequency(void);
+extern void SetFstep(int);
+extern void FminusClicked(void);
+extern void FplusClicked(void);
+extern void SetAGC(Agctype);
+extern void SetBW(Bwidth);
+extern void SetMode(Mode);
+
+extern void DisplayFrequency(void);
+extern void Touch(void);
+extern void Display(void);
+extern void Display_Init(void);
+extern void ADC_Stream0_Handler(uint8_t);
+extern void LED_switch(void);
+
+extern void DisplayStatus(void);
+extern void UserInput(void);
+void SystemClock_Config_For_OC(void);
+void MX_TIM6_Init_Custom_Rate(void);
+
+
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define USER_Btn_Pin GPIO_PIN_13
+#define USER_Btn_GPIO_Port GPIOC
+#define USER_Btn_EXTI_IRQn EXTI15_10_IRQn
+#define SwInt1_Pin GPIO_PIN_14
+#define SwInt1_GPIO_Port GPIOC
+#define SwInt1_EXTI_IRQn EXTI15_10_IRQn
+#define MCO_Pin GPIO_PIN_0
+#define MCO_GPIO_Port GPIOH
+#define RMII_MDC_Pin GPIO_PIN_1
+#define RMII_MDC_GPIO_Port GPIOC
+#define RMII_REF_CLK_Pin GPIO_PIN_1
+#define RMII_REF_CLK_GPIO_Port GPIOA
+#define RMII_MDIO_Pin GPIO_PIN_2
+#define RMII_MDIO_GPIO_Port GPIOA
+#define RMII_CRS_DV_Pin GPIO_PIN_7
+#define RMII_CRS_DV_GPIO_Port GPIOA
+#define RMII_RXD0_Pin GPIO_PIN_4
+#define RMII_RXD0_GPIO_Port GPIOC
+#define RMII_RXD1_Pin GPIO_PIN_5
+#define RMII_RXD1_GPIO_Port GPIOC
+#define RMII_TXD1_Pin GPIO_PIN_13
+#define RMII_TXD1_GPIO_Port GPIOB
+#define LD3_Pin GPIO_PIN_14
+#define LD3_GPIO_Port GPIOB
+#define STLK_RX_Pin GPIO_PIN_8
+#define STLK_RX_GPIO_Port GPIOD
+#define STLK_TX_Pin GPIO_PIN_9
+#define STLK_TX_GPIO_Port GPIOD
+#define USB_PowerSwitchOn_Pin GPIO_PIN_6
+#define USB_PowerSwitchOn_GPIO_Port GPIOG
+#define USB_OverCurrent_Pin GPIO_PIN_7
+#define USB_OverCurrent_GPIO_Port GPIOG
+#define USB_SOF_Pin GPIO_PIN_8
+#define USB_SOF_GPIO_Port GPIOA
+#define USB_VBUS_Pin GPIO_PIN_9
+#define USB_VBUS_GPIO_Port GPIOA
+#define USB_ID_Pin GPIO_PIN_10
+#define USB_ID_GPIO_Port GPIOA
+#define USB_DM_Pin GPIO_PIN_11
+#define USB_DM_GPIO_Port GPIOA
+#define USB_DP_Pin GPIO_PIN_12
+#define USB_DP_GPIO_Port GPIOA
+#define RMII_TX_EN_Pin GPIO_PIN_11
+#define RMII_TX_EN_GPIO_Port GPIOG
+#define RMII_TXD0_Pin GPIO_PIN_13
+#define RMII_TXD0_GPIO_Port GPIOG
+#define LD2_Pin GPIO_PIN_7
+#define LD2_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+#define USE_DCACHE
+
+// Select CPU and ADC Clock. Uncomment only one at a time
+//#define CLK_600M_CPU_150M_ADC
+//TODO 500 128 seems to be bad
+//#define CLK_500M_CPU_120M_ADC
+//#define CLK_500M_CPU_128M_ADC
+//#define CLK_480M_CPU_120M_ADC
+//#define CLK_600M_CPU_60M_ADC
+#define CLK_600M_CPU_160M_ADC
+
+#define BSIZE        (512)
+#define FFTLEN       (BSIZE*2)
+#define NUMFIRCOEFS  64
+#define DIRECTFFT    0
+#define INVERSEFFT   1
+#define NOREVERSE    1
+#define MAXPRESETS   14
+#define true  1
+#define false 0
+#define __FPU_PRESENT 1
+#define myPI         3.14159265358979f
+#define TWOPI        (2.f*myPI)
+#define CWPITCH      650.f
+#define SAMPLINGRATE 1785714.286f
+#define RATEOUT      (SAMPLINGRATE/64)
+
+
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
